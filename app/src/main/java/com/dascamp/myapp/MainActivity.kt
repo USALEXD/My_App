f80b7cdecfc8170c7405e53ae86f23a8361a2cb6
Alexandra <usalexd@gmail.com>
2023-03-05 16:51:43 -0800

    Module-23 Deleted menu layout; Added toolBar, navigation

--- app/src/main/java/com/dascamp/myapp/MainActivity.kt
+++ app/src/main/java/com/dascamp/myapp/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.dascamp.myapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+abstract class MainActivity(
+    private val topAppBar: MaterialToolbar,
+    private val bottom_navigation: BottomNavigationView
+) : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        bottom_navigation.setOnItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Favourite", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Watch Later", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Collections", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
